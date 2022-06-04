a8d1ea29f27ff97f41c9850c62df7450d4379ddb
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-04 15:49:28 +0545

    added a rich text editor to the admin side

--- sogi/base/models.py
+++ sogi/base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 # class User(AbstractUser):
@@ -10,10 +11,12 @@ from django.contrib.auth.models import User
 #     USERNAME_FIELD = 'email'
 #     REQUIRED_FIELDS = []
 
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    content = models.TextField()
+    # content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
 
